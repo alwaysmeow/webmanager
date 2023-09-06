51464d75d188cb73f879b8df942a73c84d77faf4
meowmeow <krivtsovsergey2003@gmail.com>
2023-09-06 17:09:14 +0300

    flask-login addition

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, url_for, request, Response, jsonify, redirect, session
+from flask import Flask, render_template, request, jsonify
+from flask_login import LoginManager, login_user, login_required
+
+from dotenv import load_dotenv
+from os import getenv
+
+from user import User
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = getenv("KEY")
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def loadUser(user_id):
+    return User(user_id)
 
 @app.route('/')
 def welcome():
@@ -9,15 +24,17 @@ def welcome():
 @app.route('/submit', methods=["POST"])
 def loginCheck():
     data = request.get_json()
-    print(data)
     if data['login'] != "meowmeow":
         response = {"success": False}
         return jsonify(response), 200
     else:
         response = {"success": True, "redirect_url": "/home"}
+        login_user(User("meowmeow"))
+        print("hello, meowmeow")
         return jsonify(response), 200
 
 @app.route('/home')
+@login_required
 def loadManager(login = "meowmeow"):
     if login:
         return render_template("home.html", title = "WebManager", login = login)
---
+++ user.py
@@ -0,0 +1,6 @@
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, id):
+        super().__init__()
+        self.id = id
