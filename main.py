72e3ead3c152ce4f17e19aca861788c96f08ac17
meowmeow <krivtsovsergey2003@gmail.com>
2023-09-21 03:35:55 +0300

    logout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, jsonify
-from flask_login import LoginManager, login_user, login_required, current_user
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 from dotenv import load_dotenv
 from os import getenv
@@ -19,10 +19,24 @@ def loadUser(user_id):
 
 @app.route('/')
 def welcome():
+    if current_user.is_authenticated:
+        url = url_for('loadManager')
+    else:
+        url = url_for('loadLoginPage')
+    return redirect(url, 301)
+
+@app.route('/login')
+def loadLoginPage():
     return render_template("login.html", title = "Log In")
 
+@app.route('/home')
+@login_required
+def loadManager():
+    print(current_user.id)
+    return render_template("home.html", title = "WebManager", login = current_user.id)
+
 @app.route('/submit', methods=["POST"])
-def loginCheck():
+def authorization():
     data = request.get_json()
     if data['login'] == "meowmeow":
         response = {"success": True, "redirect_url": "/home"}
@@ -36,11 +50,31 @@ def loginCheck():
         response = {"success": False}
         return jsonify(response), 200
 
-@app.route('/home')
+@app.route('/logout', methods=["GET"])
 @login_required
-def loadManager():
-    print(current_user.id)
-    return render_template("home.html", title = "WebManager", login = current_user.id)
+def logoutCurrentUser():
+    logout_user()
+    response = {"success": True, "redirect_url": "/login"}
+    return jsonify(response)
+
+"""
+@app.route('/data', methods=["GET"])
+@login_required
+def throwData():
+    response = {
+        "user": current_user.id,
+        "categories": {
+            "studying": {
+                "MPEI": "https://mpei.ru/"
+            },
+            "social": {
+                "VK": "https://vk.com/",
+                "YouTube": "https://www.youtube.com/"
+            }
+        }
+    }
+    return jsonify(response), 200
+"""
 
 if __name__ == "__main__":
     app.run(debug=True, host="192.168.0.14",port="1820")
---
+++ static/scripts/base.js
@@ -0,0 +1,20 @@
+function logout()
+{
+    request = {
+        method: "GET"
+    }
+    
+    fetch('/logout', request)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        if (data.success)
+        {
+            window.location.href = data.redirect_url
+        }
+    })
+}
+
+logoutButton = document.getElementById("logout-button")
+logoutButton.addEventListener('click', logout)
