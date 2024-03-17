bee4e9c90c1d00a71a8dc42984fd5686c9fba409
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2024-03-17 17:47:13 +0300

    flask prod server

--- server/run.py
+++ server/run.py
@@ -1,5 +1,7 @@
 from app import app
 from api import *
+from waitress import serve
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="8000")
+    #app.run(debug=True, host="0.0.0.0", port="8000")
+    serve(app, host="0.0.0.0", port="8000")
