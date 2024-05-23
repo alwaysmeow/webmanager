80a3665fc765ac63d68eb3092dd0b69843ed7b7a
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2024-05-23 23:02:32 +0300

    server hosting

--- server/api/run.py
+++ server/api/run.py
@@ -3,5 +3,5 @@ from api import *
 from waitress import serve
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="8000")
-    #serve(app, host="0.0.0.0", port="8000")
+    #app.run(debug=True, host="0.0.0.0", port="8000")
+    serve(app, host="0.0.0.0", port="8000")
